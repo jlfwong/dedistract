30e7807ef1faeb67da4cb1f31e2dfcb07ea460d5
Jamie Wong <jamie.lf.wong@gmail.com>
2020-09-07 12:55:22 -0700

    Allow looking at photos in FB feed without reblocking

--- fb-block.js
+++ fb-block.js
@@ -1,1 +1,20 @@
-enableFeedOnPathsOtherThan(["/"])
+let lastPath = null
+addPathChangeListener((path) => {
+  if (path === "/") {
+    if (document.querySelector(".disable-dedistract") && lastPath && lastPath.indexOf("photo") !== -1) {
+      // Special case: when you're looking through the feed and click on a photo, it changes
+      // the path to something like "/photos?id=..." or "/groupname/photos/..."
+      //
+      // When you close the photo it reverts back to "/". It's annoying to bring
+      // back the block immediately when you do this, so we'll just do nothing
+      // in this case.
+    } else {
+      console.log("[dedistract] blocking feed for", path)
+      setEnabled(true)
+    }
+  } else {
+    console.log("[dedistract] unblocking feed for", path)
+    setEnabled(false)
+  }
+  lastPath = path
+})
